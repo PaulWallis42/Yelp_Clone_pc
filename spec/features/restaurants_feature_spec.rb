0d86d1a46f9390286360dd9de0cb40d0f265363e
paulwallis42 <paulwallis42@gmail.com>
2015-07-25 07:21:44 +0100

    new restaurant form added

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -5,7 +5,7 @@ feature 'restaurants' do
     scenario 'should dispay a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
-      expect(page).to have_link 'Add a restaurant'
+      expect(page).to have_link 'Add a Restaurant'
     end
   end
 
@@ -18,6 +18,17 @@ feature 'restaurants' do
       visit '/restaurants'
       expect(page).to have_content('KFC')
       expect(page).not_to have_content('No restaurants yet')
+    end
+  end
+
+  context 'creating restaurants' do
+    scenario 'prompts user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a Restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
     end
   end
 end
