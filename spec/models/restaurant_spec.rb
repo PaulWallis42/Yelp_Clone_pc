2d682e3e2fcd31bcf1253085c19c2a0e1fe2236f
paulwallis42 <paulwallis42@gmail.com>
2015-07-27 14:45:22 +0100

    review belongs to restaurants

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -9,3 +9,7 @@ require 'spec_helper'
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
 end
+
+describe Review, type: :model do
+  it { is_expected.to belong_to :restaurant }
+end
