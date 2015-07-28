1cb68660dea8fcc689de400e388cb98a1be07998
paulwallis42 <paulwallis42@gmail.com>
2015-07-28 09:52:38 +0100

    facebook sign in not working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
 
-  root to: 'restaurants#index'
+  # root to: 'restaurants#index'
+  #
+  # devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # resources :restaurants do
+  #   resources :reviews
+  # end
 
-  devise_for :users
+  # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+
+  root to: "restaurants#index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  # devise_scope :user do
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
+
   resources :restaurants do
     resources :reviews
   end
